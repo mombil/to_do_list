03bd451a872786954f047b542556cea8eb86f526
Pawel Brejnak <pawelbrejnak6@gmail.com>
2023-02-17 18:05:49 +0100

    added Readme

---
+++ JS/README.md
@@ -0,0 +1,16 @@
+# To do list
+## Description
+
+
+
+## Demo
+
+[To do list](https://mombil.github.io/to_do_list/)
+
+![Demo photo](images/demo.png)
+
+## Technologies
+- Grid
+- CSS
+- ES6+ feature
+- BEM
--- JS/javascript.js
+++ JS/javascript.js
@@ -1,94 +1,101 @@
 {
-    const tasks = [
-        {
-            content : " Pograj w Cs'a",
-            done : true,
-        },
-        {
-            content : "Poucz się programować",
-            done: false,
-        }
-    ]
+	const tasks = [
+		{
+			content: " Pograj w Cs'a",
+			done: true,
+		},
+		{
+			content: "Poucz się programować",
+			done: false,
+		},
+	]
 
-    const taskRemove = (index) => {
-        tasks.splice(index, 1)
-    render()
-    }
-    
-    const taskDone = (index) =>{
-        tasks[index].done = !tasks[index].done
-        render()
-    } 
+	const taskRemove = index => {
+		tasks.splice(index, 1)
+		render()
+	}
 
-    const addNewContent = (newTask) => {
-        tasks.push({
-            content : newTask,
-        }) 
-    }
+	const taskDone = index => {
+		tasks[index].done = !tasks[index].done
+		render()
+	}
 
-    const render = () => {
-        let htmlString = ""
-        for (const task of tasks) {
-            htmlString += `
+	const addNewContent = newTask => {
+		tasks.push({
+			content: newTask,
+		})
+	}
+
+	const render = () => {
+		let htmlString = ""
+		for (const task of tasks) {
+			htmlString += `
             <li class="list__item ">
-            <button class="js-done list__button list__button--done">${task.done ? "✔" : ""}</button>
-            <span class="${task.done ? "list__item--done" : ""}">${task.content}</span>
+            <button class="js-done list__button list__button--done">${
+							task.done ? "✔" : ""
+						}</button>
+            <span class="${task.done ? "list__item--done" : ""}">${
+				task.content
+			}</span>
             <button class="js-remove list__button list__button--remove">🗑</button>
             </li>
-            `          
-        }
-        document.querySelector(".js-listTasks").innerHTML = htmlString
+            `
+		}
+		document.querySelector(".js-listTasks").innerHTML = htmlString
 
-        removeButtons = document.querySelectorAll(".js-remove")
-        
-        removeButtons.forEach((removeButtons, index) => {
-            removeButtons.addEventListener("click", () => {
-                
-                taskRemove(index)
+		removeButtons = document.querySelectorAll(".js-remove")
 
-            })
-        });
+		removeButtons.forEach((removeButtons, index) => {
+			removeButtons.addEventListener("click", () => {
+				taskRemove(index)
+			})
+		})
 
-        doneButtons = document.querySelectorAll(".js-done")
-        
-        doneButtons.forEach((doneButtons, index) => {
-            doneButtons.addEventListener("click", () => {
-                taskDone(index)
+		doneButtons = document.querySelectorAll(".js-done")
 
-            })
-        });
-    }
+		doneButtons.forEach((doneButtons, index) => {
+			doneButtons.addEventListener("click", () => {
+				taskDone(index)
+			})
+		})
+	}
 
-    const onFormSubmit = (event) => {
-        event.preventDefault()
+	const cleanInput = () => {
+		document.querySelector(".js-addTaskInput").value = ""
+	}
+	const autoFocus = () => {
+		document.querySelector(".js-addTaskInput").focus()
+	}
 
-        newTask = document.querySelector(".js-addTaskInput").value.trim()
-        
-        if (!newTask){
-            return
-        }
+	const onFormSubmit = event => {
+		event.preventDefault()
 
-        addNewContent(newTask)
-        
-        document.querySelector(".js-addTaskInput").value = ""
-        document.querySelector(".js-addTaskInput").focus()
+		newTask = document.querySelector(".js-addTaskInput").value.trim()
 
-        
-        render()
-    }
+		if (!newTask) {
+			return
+		}
 
-    const addNewTask = () => {
-        form = document.querySelector(".js-form")
+		addNewContent(newTask)
 
-        form.addEventListener("submit", (event) => {
-            onFormSubmit(event)
-        })
-    } 
+		cleanInput()
+		autoFocus()
 
-    const init = () => {
-        addNewTask()
-        render()
-    }
+		render()
+	}
 
-    init()
+	const addNewTask = () => {
+		form = document.querySelector(".js-form")
+
+		form.addEventListener("submit", event => {
+			onFormSubmit(event)
+		})
+	}
+
+	const init = () => {
+		addNewTask()
+		render()
+	}
+
+	init()
 }
