b8991053d1642f2f05a4bb0e863ce429fdfbfe40
Pawel Brejnak <pawelbrejnak6@gmail.com>
2023-02-17 20:00:47 +0100

    code cleaning

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,94 +1,92 @@
 {
-	const tasks = [
-        
-    ]
+	const tasks = [];
 
 	const taskRemove = index => {
-		tasks.splice(index, 1)
-		render()
-	}
+		tasks.splice(index, 1);
+		render();
+	};
 
 	const taskDone = index => {
-		tasks[index].done = !tasks[index].done
-		render()
-	}
+		tasks[index].done = !tasks[index].done;
+		render();
+	};
 
 	const addNewContent = newTask => {
 		tasks.push({
 			content: newTask,
-		})
-	}
+		});
+	};
 
 	const render = () => {
-		let htmlString = ""
+		let htmlString = "";
 		for (const task of tasks) {
 			htmlString += `
             <li class="list__item ">
             <button class="js-done list__button list__button--done">${
 							task.done ? "✔" : ""
 						}</button>
-            <span class="${task.done ? "list__item--done" : ""}">${
+            <span class="${task.done ? " list__text" : ""}">${
 				task.content
 			}</span>
             <button class="js-remove list__button list__button--remove">🗑</button>
             </li>
-            `
-		}
-		document.querySelector(".js-listTasks").innerHTML = htmlString
+            `;
+		};
+		document.querySelector(".js-listTasks").innerHTML = htmlString;
 
-		removeButtons = document.querySelectorAll(".js-remove")
+		removeButtons = document.querySelectorAll(".js-remove");
 
 		removeButtons.forEach((removeButtons, index) => {
 			removeButtons.addEventListener("click", () => {
-				taskRemove(index)
-			})
-		})
+				taskRemove(index);
+			});
+		});
 
-		doneButtons = document.querySelectorAll(".js-done")
+		doneButtons = document.querySelectorAll(".js-done");
 
 		doneButtons.forEach((doneButtons, index) => {
 			doneButtons.addEventListener("click", () => {
-				taskDone(index)
-			})
-		})
-	}
+				taskDone(index);
+			});
+		});
+	};
 
 	const cleanInput = () => {
-		document.querySelector(".js-addTaskInput").value = ""
-	}
+		document.querySelector(".js-addTaskInput").value = "";
+	};
 	const autoFocus = () => {
-		document.querySelector(".js-addTaskInput").focus()
-	}
+		document.querySelector(".js-addTaskInput").focus();
+	};
 
 	const onFormSubmit = event => {
-		event.preventDefault()
+		event.preventDefault();
 
-		newTask = document.querySelector(".js-addTaskInput").value.trim()
+		newTask = document.querySelector(".js-addTaskInput").value.trim();
 
 		if (!newTask) {
-			return
-		}
+			return;
+		};
 
-		addNewContent(newTask)
+		addNewContent(newTask);
 
-		cleanInput()
-		autoFocus()
+		cleanInput();
+		autoFocus();
 
-		render()
-	}
+		render();
+	};
 
 	const addNewTask = () => {
-		form = document.querySelector(".js-form")
+		form = document.querySelector(".js-form");
 
 		form.addEventListener("submit", event => {
-			onFormSubmit(event)
-		})
-	}
+			onFormSubmit(event);
+		});
+	};
 
 	const init = () => {
-		addNewTask()
-		render()
-	}
+		addNewTask();
+		render();
+	};
 
-	init()
+	init();
 }
